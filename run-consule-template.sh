33396d2c87244569d173acf14fdb8b68d77763e0
Michael Elsdörfer <michael@elsdoerfer.com>
2015-04-20 16:01:40 +0200

    Wait 1s before writing, disable debug.

--- run-consule-template.sh
+++ run-consule-template.sh
@@ -4,4 +4,4 @@ HEARTBEAT=30
 TTL=60
 
 watch -n $HEARTBEAT sh etcd-set &
-exec /consul-template -consul $CONSUL -template "/etcd-set.ctmpl:/etcd-set:sh /etcd-set" -retry 30s  --log-level debug $@
+exec /consul-template -consul $CONSUL -template "/etcd-set.ctmpl:/etcd-set:sh /etcd-set" -retry 30s -wait 1s  --log-level debug $@
