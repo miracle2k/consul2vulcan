d54309619beab27322c4ec0ea94d47df452a7afa
Michael Elsdörfer <michael@elsdoerfer.com>
2015-05-03 16:08:58 +0200

    Debug log level should not be the default.

--- run-consule-template.sh
+++ run-consule-template.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-HEARTBEAT=30
-TTL=60
+export HEARTBEAT=30
 
 watch -n $HEARTBEAT sh etcd-set &
-exec /consul-template -consul $CONSUL -template "/etcd-set.ctmpl:/etcd-set:sh /etcd-set" -retry 30s -wait 1s  --log-level debug $@
+exec /consul-template -consul $CONSUL -template "/etcd-set.ctmpl:/etcd-set:sh /etcd-set" -retry 30s -wait 1s $@
